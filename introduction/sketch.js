7f267bb3049579f9a439c043f9364fd367185b98
Chakkapat <tinetine4554@gmail.com>
2017-08-02 16:34:28 +0700

    สร้างกราฟ

--- introduction/sketch.js
+++ introduction/sketch.js
@@ -5,6 +5,8 @@ function setup() {
 function draw() {
     background(0, 255, 0)
     var size = 100;
+    fill(255,0,0);
+    rect(0,0,255,255)
     rect(width/2 - size/2, 
          height/2-size/2, size, size)
 }
